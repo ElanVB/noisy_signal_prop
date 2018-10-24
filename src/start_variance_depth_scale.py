824c1209fc47eef0556fa69398f2a53e5ce1e800
Elan van Biljon <elan@vanbiljon.co.za>
2018-10-24 15:25:02 +0200

    Change variance depth test params

--- src/start_variance_depth_scale.py
+++ src/start_variance_depth_scale.py
@@ -4,6 +4,6 @@ import numpy as np
 from variance_depth_scale import test_init
 
 # any more fine grained and this line MUST CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-inits = np.round(np.linspace(0.1, 2.5, 24), 1)
+inits = np.linspace(0.1, 2.5, 25)
 
 test_init(inits[int(sys.argv[1])])

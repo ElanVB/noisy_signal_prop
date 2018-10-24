a230857ccdb0c3f9f5621a95cf7663f9c5be0b7e
Elan van Biljon <elan@vanbiljon.co.za>
2018-10-24 08:15:27 +0200

    Change variance to use cifar10 and more inits

--- src/start_variance_depth_scale.py
+++ src/start_variance_depth_scale.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from variance_depth_scale import test_init
 
-inits = np.round(np.linspace(0.1, 2.5, 12), 1)
+# any more fine grained and this line MUST CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+inits = np.round(np.linspace(0.1, 2.5, 24), 1)
 
 test_init(inits[int(sys.argv[1])])
